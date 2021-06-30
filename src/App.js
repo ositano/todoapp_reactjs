7c866ed0ca8961a9cfc66af5f183a207ffdc9a37
Green <green.onyeji@gmail.com>
2021-06-30 09:40:38 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import SubmitForm from './components/SubmitForm/SubmitForm';
+import TasksList from './components/TasksList/TasksList'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  
+  constructor(props){
+    super(props);
+    this.state ={tasks: []}
+
+    this.addTask = this.addTask.bind(this);
+    this.removeTask = this.removeTask.bind(this);
+  }
+
+  addTask(task){
+    this.setState((prevState) => {
+      return { 
+        tasks: prevState.tasks.concat(task) 
+      };
+    });
+  }
+
+  removeTask(id){
+      const tasks = this.state.tasks.filter(element => (element.id !== id));
+      this.setState({ tasks: tasks });
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <SubmitForm addTask={this.addTask}/>
+        <TasksList tasks={this.state.tasks} removeTask={this.removeTask}/>
+      </div>
+    );
+  }
 }
 
 export default App;
